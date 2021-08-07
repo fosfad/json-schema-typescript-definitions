f866985a1b6467e249de2f9d42afb159d8146b54
Petr <petr@flaks.dev>
2021-08-08 01:23:09 +0300

    Export reference

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {
-  JsonSchema, AnyJsonSchema, ObjectJsonSchema, ArrayJsonSchema, IntegerJsonSchema, NumberJsonSchema, StringJsonSchema, BooleanJsonSchema, NullJsonSchema,
-  isAnyJsonSchema, isObjectJsonSchema, isArrayJsonSchema, isIntegerJsonSchema, isNumberJsonSchema, isStringJsonSchema, isBooleanJsonSchema, isNullJsonSchema,
+  Reference, JsonSchema, AnyJsonSchema, ObjectJsonSchema, ArrayJsonSchema, IntegerJsonSchema, NumberJsonSchema, StringJsonSchema, BooleanJsonSchema, NullJsonSchema,
+  isReference, isAnyJsonSchema, isObjectJsonSchema, isArrayJsonSchema, isIntegerJsonSchema, isNumberJsonSchema, isStringJsonSchema, isBooleanJsonSchema, isNullJsonSchema,
 } from './jsonSchema';
