60694f4ca4889e04474e879d1d9a5006acd378e4
Petr <petr@flaks.dev>
2021-09-11 18:30:28 +0300

    Refactor the package to support multiple JSON Schema drafts

---
+++ src/2020-12.ts
@@ -0,0 +1,100 @@
+type CoreKeywords = {
+  $anchor?: string;
+  $comment?: string;
+  $defs?: { [key: string]: JsonSchema };
+  $dynamicAnchor?: string;
+  $dynamicRef?: string;
+  $id?: string;
+  $ref?: string;
+  $schema?: string;
+  $vocabulary?: { [uri: string]: boolean };
+};
+
+type LogicSubschemasKeywords = {
+  allOf?: Array<JsonSchema>;
+  anyOf?: Array<JsonSchema>;
+  not?: JsonSchema;
+  oneOf?: Array<JsonSchema>;
+};
+
+type ConditionalSubschemasKeywords = {
+  dependentSchemas?: { [key: string]: JsonSchema };
+  else?: JsonSchema;
+  if?: JsonSchema;
+  then?: JsonSchema;
+};
+
+type JsonSchemaType = 'null' | 'boolean' | 'string' | 'integer' | 'number' | 'array' | 'object';
+
+type AnyValidationKeywords = {
+  const?: any;
+  enum?: Array<any>;
+  type?: JsonSchemaType | Array<JsonSchemaType>;
+};
+
+type MetaDataAnnotationKeywords = {
+  default?: any;
+  deprecated?: boolean;
+  description?: string;
+  examples?: Array<any>;
+  readOnly?: boolean;
+  title?: string;
+  writeOnly?: boolean;
+};
+
+type StringValidationKeywords = {
+  maxLength?: number;
+  minLength?: number;
+  pattern?: string;
+};
+
+type NumericValidationKeywords = {
+  exclusiveMaximum?: number;
+  exclusiveMinimum?: number;
+  maximum?: number;
+  minimum?: number;
+  multipleOf?: number;
+};
+
+type ArrayValidationKeywords = {
+  contains?: JsonSchema;
+  items?: JsonSchema;
+  maxContains?: number;
+  maxItems?: number;
+  minContains?: number;
+  minItems?: number;
+  prefixItems?: Array<JsonSchema>;
+  uniqueItems?: boolean;
+};
+
+type ObjectValidationKeywords = {
+  dependentRequired?: Record<string, Array<string>>;
+  maxProperties?: number;
+  minProperties?: number;
+  required: string[];
+};
+
+type ObjectSubschemaKeywords = {
+  additionalProperties?: JsonSchema;
+  patternProperties?: { [propertyNameRegex: string]: JsonSchema };
+  properties?: { [propertyName: string]: JsonSchema };
+  propertyNames?: JsonSchema;
+};
+
+type UnevaluatedLocationsKeywords = {
+  unevaluatedItems?: JsonSchema;
+  unevaluatedProperties?: JsonSchema;
+};
+
+export type JsonSchema = CoreKeywords &
+  LogicSubschemasKeywords &
+  ConditionalSubschemasKeywords &
+  JsonSchemaType &
+  AnyValidationKeywords &
+  MetaDataAnnotationKeywords &
+  StringValidationKeywords &
+  NumericValidationKeywords &
+  ArrayValidationKeywords &
+  ObjectValidationKeywords &
+  UnevaluatedLocationsKeywords &
+  ObjectSubschemaKeywords;
